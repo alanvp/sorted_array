2516d870c46a96c42dd17a213a741543d79f5c06
alanvp <apvanpelt@yahoo.com>
2014-01-13 19:44:34 -0800

    finished

--- sorted_array.rb
+++ sorted_array.rb
@@ -3,13 +3,10 @@ class SortedArray
 
   def initialize(input_arr=[])
     @internal_arr = []
-    # Fill in the rest of the initialize method here.
-    # What should you do with each element of the incoming array?
     input_arr.each {|elem| self.add(elem)}
   end
 
   def add(new_elem)
-    # Hint: Use the Array#insert method.
     @internal_arr.insert(first_larger_index(new_elem, 0, @internal_arr.size), new_elem)
  #   raise NotImplementedError.new("You need to implement the add method!")
   end
@@ -19,13 +16,8 @@ class SortedArray
   end
 
   def [](index)
- #   if index > @internal_arr.length then return nil end
- #   if index.is_a?(Array)
       return @internal_arr[index]
- #   return @internal_arr[index]
-  
 #   raise NotImplementedError.new("You need to implement the [] method!")
-  
   end
 
   def first_larger_index(target, start_ind=0, end_ind=@internal_arr.size)
